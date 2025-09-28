19ccafb6bb0f76431bf5cf10876eb0e07fc34c20
Jong-Chan Kim <jongchank@gmail.com>
2025-09-28 15:33:05 +0900

    Add coding agents

--- setup.sh
+++ setup.sh
@@ -9,8 +9,23 @@ sudo apt -y install gh
 sudo apt -y install valgrind 
 sudo apt -y install cmake
 sudo apt -y install xutils-dev
+
+# Enabling core 
+sudo sysctl -w kernel.core_pattern=core
+
+# Installing lazygit
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
 sudo install lazygit -D -t /usr/local/bin/
-sudo sysctl -w kernel.core_pattern=core
+
+# Installing coding agents
+#curl -fsSL https://deb.nodesource.com/setup_24.x | sudo -E bash -
+#sudo apt install nodejs
+#mkdir ~/.npm-global
+#npm config set prefix '~/.npm-global'
+#echo 'export PATH=~/.npm-global/bin:$PATH' >> ~/.bashrc
+#source ~/.bashrc
+#npm install -g @anthropic-ai/claude-code
+#npm install -g @google/gemini-cli
+#npm install -g @openai/codex
