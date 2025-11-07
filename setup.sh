c253e8043ab36d0f7c911866d65caee2b58e18ed
Jong-Chan Kim <jongchank@gmail.com>
2025-11-07 23:23:51 +0900

    Remove agent installations from setup.sh

--- setup.sh
+++ setup.sh
@@ -17,15 +17,4 @@ sudo sysctl -w kernel.core_pattern=core
 LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
 curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
 tar xf lazygit.tar.gz lazygit
-sudo install lazygit -D -t /usr/local/bin/
-
-# Installing coding agents
-#curl -fsSL https://deb.nodesource.com/setup_24.x | sudo -E bash -
-#sudo apt install nodejs
-#mkdir ~/.npm-global
-#npm config set prefix '~/.npm-global'
-#echo 'export PATH=~/.npm-global/bin:$PATH' >> ~/.bashrc
-#npm install -g @anthropic-ai/claude-code
-#npm install -g @google/gemini-cli
-#npm install -g @openai/codex
-#echo "Please $source ~/.bashrc"
+sudo install lazygit -D-t /usr/local/bin/
